8ef6d18a553735140c8d23c917dfb0849d816800
axcoder <axsimms@yahoo.com>
2025-11-19 00:24:53 -0500

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -10,6 +10,7 @@ export const sharedPageComponents: SharedLayout = {
     links: {
       GitHub: "https://github.com/axellabs",
       LinkedIn: "https://www.linkedin.com/in/axelsimms",
+      Resume: "https://axelsimms.com/static/resume.pdf"
     },
   }),
 }
