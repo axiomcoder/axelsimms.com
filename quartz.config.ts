5fda53acdb91e079888202a7ef1ade109deacc30
axcoder <axsimms@yahoo.com>
2025-11-19 00:33:11 -0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "axelsimms.com",
+    baseUrl: "axelsimms-com.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
